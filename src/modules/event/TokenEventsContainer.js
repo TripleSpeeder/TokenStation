f898101141b67477b23acfa814d2913ad54ee6e0
Michael Bauer <michael@m-bauer.org>
2018-03-06 17:03:51 +0100

    When looking at transfer events of a contract, make sure that all addressContractEvent entries have the same block range checked.

--- src/modules/event/TokenEventsContainer.js
+++ src/modules/event/TokenEventsContainer.js
@@ -26,7 +26,6 @@ class TokenEventsContainer extends Component {
         )
     }
 
-
     checkEventsLoaded(props) {
         /*
         For each address check if ace-entry is existing, Collect all missing addresses.
@@ -37,7 +36,10 @@ class TokenEventsContainer extends Component {
         the same blockrange checked.
         */
         if (props.web3 && props.missingAceEntryAddresses.length) {
-            props.loadTokenTransferEvents(0, 0, props.token.id, props.missingAceEntryAddresses)
+            const firstBlock = props.minStart === Number.MAX_SAFE_INTEGER ? 0
+                : props.minStart
+            const lastBlock = props.maxEnd
+            props.loadTokenTransferEvents(firstBlock, lastBlock, props.token.id, props.missingAceEntryAddresses)
         }
 
     }
@@ -70,10 +72,50 @@ const mapStateToProps = (state, ownProps) => {
     const transferEventIds = token.eventIds
     const etherscanUrl = buildEtherscanLink(token.address)
 
-    // check if any addressContractEvents are not yet loaded
+    // Align the checked block range for each existing ace entry
+    // To do this, first determine the maximum values of all ace entries
+    let minStart = Number.MAX_SAFE_INTEGER
+    let maxEnd = 0
+    state.addresses.allIds.forEach(addressId => {
+        const aceId = buildAdressContractEventId(addressId, tokenId)
+        const aceEntry = state.events.aceById[aceId]
+        if (aceEntry && (!aceEntry.isLoading) && (aceEntry.firstBlock > 0)) {
+            // okay, entry existing and initialized. Now look at checked block range
+            minStart = Math.min(minStart, aceEntry.firstBlock)
+            maxEnd = Math.max(maxEnd, aceEntry.lastBlock)
+        }
+    })
+
+    /*
+        This could be further optimized by calculating the exact missing range for each
+        ace entry. Right now I'm just determining the min/max values and for each entry
+        that is incomplete just request the full range of blocks.
+     */
+
+    // check if any addressContractEvents are missing
     const missingAceEntryAddresses = state.addresses.allIds.filter(entry => {
         const aceId = buildAdressContractEventId(entry, tokenId)
-        return (state.events.aceById[aceId] === undefined)
+        const aceEntry = state.events.aceById[aceId]
+        if (aceEntry === undefined) {
+            // entry not existing at all
+            return true
+        }
+        if (aceEntry.isLoading) {
+            // ignore for now, will recheck once it's loaded
+            return false
+        }
+        if (aceEntry.firstBlock === 0) {
+            // Freshly created, will recheck once it's loaded
+            return false
+        }
+        if ((aceEntry.firstBlock > minStart) || (aceEntry.lastBlock < maxEnd)) {
+            // incomplete range
+            // console.log("AceEntry " + aceId + " has incomplete range " + aceEntry.firstBlock + ' - ' + aceEntry.lastBlock)
+            // console.log("Expected range: " + minStart + " - " + maxEnd)
+            return true
+        }
+        // valid entry that has full range covered. Fine :-)
+        return false
     })
 
     return {
@@ -81,7 +123,9 @@ const mapStateToProps = (state, ownProps) => {
         token: token,
         etherscanUrl: etherscanUrl,
         missingAceEntryAddresses,
-        transferEventIds
+        transferEventIds,
+        minStart,
+        maxEnd
     }
 }
 const mapDispatchToProps = dispatch => ({
