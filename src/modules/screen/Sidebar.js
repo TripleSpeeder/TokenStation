3ae4880f133140b781af25e116a36993173643f8
Michael Bauer <michael@m-bauer.org>
2018-05-03 19:20:34 +0200

    Add sidebar

---
+++ src/modules/screen/Sidebar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Icon, Menu} from 'semantic-ui-react'
+
+
+const Sidebar = (props) => {
+    const {activeItem, clickItem} = props
+
+    const handleItemClick = (e, { name }) => clickItem(name)
+
+    return (
+        <Menu icon='labeled' inverted vertical pointing size={'large'}>
+            <Menu.Item name='overview' active={activeItem === 'overview'} onClick={handleItemClick}>
+                <Icon name='home' />
+                Overview
+            </Menu.Item>
+
+            <Menu.Item name='accounts' active={activeItem === 'accounts'} onClick={handleItemClick}>
+                <Icon name='users' />
+                Manage Accounts
+            </Menu.Item>
+
+            <Menu.Item name='token' active={activeItem === 'token'} onClick={handleItemClick}>
+                <Icon name='cube' />
+                Manage Contracts
+            </Menu.Item>
+        </Menu>
+    )
+}
+
+Sidebar.propTypes = {
+    activeItem: PropTypes.string.isRequired,
+    clickItem: PropTypes.func.isRequired
+}
+
+Sidebar.defaultProps = {
+    //myProp: <defaultValue>
+}
+
+export default Sidebar
---
+++ src/modules/screen/Sidebar.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+import {action} from '@storybook/addon-actions'
+import StoryRouter from 'storybook-react-router'
+import Sidebar from './Sidebar'
+
+const actions = {
+    clickSidebar: action('clickSidebar')
+}
+
+storiesOf('Sidebar', module)
+    .add('overview', () => <Sidebar activeItem={'overview'} clickItem={actions.clickSidebar}/>)
+    .add('accounts', () => <Sidebar activeItem={'accounts'} clickItem={actions.clickSidebar}/>)
+    .add('token', () => <Sidebar activeItem={'token'} clickItem={actions.clickSidebar}/>)
+
