3b5b4de8e7ff680e893d01a50efc1c6e7f667914
Michael Bauer <michael@m-bauer.org>
2018-05-03 19:22:34 +0200

    Add sidebar

--- src/modules/screen/Sidebar.stories.js
+++ src/modules/screen/Sidebar.stories.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
 import {action} from '@storybook/addon-actions'
-import StoryRouter from 'storybook-react-router'
 import Sidebar from './Sidebar'
 
 const actions = {
