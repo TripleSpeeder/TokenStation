057ee01164785be4b40b75b2fd3d149f02d5c046
Michael Bauer <michael@m-bauer.org>
2018-06-05 22:24:18 +0200

    Show message when no token contracts available.

--- src/modules/token/SelectableTokenList.js
+++ src/modules/token/SelectableTokenList.js
@@ -1,20 +1,29 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types';
-import {Table, Form} from 'semantic-ui-react'
+import {Table, Form, Message} from 'semantic-ui-react'
 import SelectableTokenContainer from "./SelectableTokenContainer"
 
 const SelectableTokenList = (props) => {
 
     const {tokenList} = props
 
-    return (
-        <Table celled>
-            {tokenList.map((tokenId) => <SelectableTokenContainer
-                key={tokenId}
-                tokenId={tokenId}
-            />)}
-        </Table>
-    )
+    if (tokenList.length) {
+        return (
+            <Table celled>
+                {tokenList.map((tokenId) => <SelectableTokenContainer
+                    key={tokenId}
+                    tokenId={tokenId}
+                />)}
+            </Table>
+        )
+    } else {
+        return (
+            <Message negative>
+                <Message.Header>No token contracts available</Message.Header>
+                <p>Try clearing the filter string.</p>
+            </Message>
+        )
+    }
 }
 
 SelectableTokenList.propTypes = {
