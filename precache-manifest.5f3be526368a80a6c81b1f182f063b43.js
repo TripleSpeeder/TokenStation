90d43123147462cdf0fcd542f680b0a56e6426ba
Michael Bauer <michael@m-bauer.org>
2018-10-21 17:52:17 +0200

    Updates

---
+++ precache-manifest.5f3be526368a80a6c81b1f182f063b43.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6dfa89a5b5ebc734796",
+    "url": "/static/js/main.f6dfa89a.chunk.js"
+  },
+  {
+    "revision": "15e379c5d9814fbc9630",
+    "url": "/static/js/1.15e379c5.chunk.js"
+  },
+  {
+    "revision": "e93d955b0590ad9432030382fd3bd4f0",
+    "url": "/index.html"
+  }
+];
