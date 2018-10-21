3aeb534da05ae3498dc92a422e9a29fe362083a5
Michael Bauer <michael@m-bauer.org>
2018-10-21 15:23:07 +0200

    Fix deprecation warning by StoryBook

--- src/modules/event/TransferEvent.stories.js
+++ src/modules/event/TransferEvent.stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {storiesOf, action} from '@storybook/react'
+import {storiesOf} from '@storybook/react'
+import {action} from "@storybook/addon-actions"
 import BigNumber from 'bignumber.js'
 import TransferEvent, {TRANSFER_EVENT_TYPES} from './TransferEvent'
 import {Table} from 'semantic-ui-react'
