05d66c0a3c0f9844531e14510bca0c17d57389f6
Michael Bauer <michael@m-bauer.org>
2018-10-24 11:19:04 +0200

    Clear selected token when network is changing

--- src/modules/token/reducers/SelectorReducer.js
+++ src/modules/token/reducers/SelectorReducer.js
@@ -1,6 +1,5 @@
-import {
-    CHANGE_SELECTOR_TOKENID, CLEAR_TOKEN_LIST,
-} from '../tokenActions'
+import {CHANGE_SELECTOR_TOKENID} from '../tokenActions'
+import {SET_NETWORK} from '../../web3/web3Actions'
 
 const SELECTOR_INITIAL = {
     selctedTokenId: undefined
@@ -16,16 +15,25 @@ function changeSelectorTokenId(state, action) {
     }
 }
 
-function clearSelectorTokenId() {
-    return SELECTOR_INITIAL
+function setNetwork(state, action) {
+    const {id, previousId} = action
+    // if the existing network got changed, clear the selected token. If this
+    // is the initial setNetwork event after page load (previousID is undefined)
+    // keep the selectedTokenId
+    // NOTE: This will only start working when Metamask stops the full page reload on network change.
+    // See https://github.com/MetaMask/metamask-extension/issues/3599.
+    if (previousId && (previousId !== id))
+        return SELECTOR_INITIAL
+    else
+        return state
 }
 
 export const selectorReducer = (state=SELECTOR_INITIAL, action) => {
     switch (action.type) {
         case CHANGE_SELECTOR_TOKENID:
             return changeSelectorTokenId(state, action)
-        case CLEAR_TOKEN_LIST:
-            return clearSelectorTokenId()
+        case SET_NETWORK:
+            return setNetwork(state, action)
         default:
             return state
     }
