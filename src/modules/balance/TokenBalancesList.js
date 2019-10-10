c4c2c0146b12bb4b43be8b0153233a676b7b1a49
Michael Bauer <michael@m-bauer.org>
2019-10-10 12:17:59 +0200

    Fix import name

--- src/modules/balance/TokenBalancesList.js
+++ src/modules/balance/TokenBalancesList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import BalanceItemContainer from './TokenBalanceItemContainer'
+import TokenBalanceItemContainer from './TokenBalanceItemContainer'
 
 const TokenBalancesList = (props) => {
     const {balancesByToken} = props
     return (
         <div>
             {balancesByToken.map(entry =>
-                <BalanceItemContainer key={entry[0]} tokenId={entry[0]} tokenBalances={entry[1]}/>
+                <TokenBalanceItemContainer key={entry[0]} tokenId={entry[0]} tokenBalances={entry[1]}/>
             )}
         </div>
     )
